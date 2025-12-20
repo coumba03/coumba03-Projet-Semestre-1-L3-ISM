52ae1dc422225ef1708abf329992d56ef34a8d4f
coumba03 <mame-coumba.sall@ism.edu.sn>
2025-12-20 18:05:28 +0000

    Supprimer tous les fichiers du dossier Models

--- Models/CatalogViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace brasil.Models
-{
-    public class CatalogViewModel
-    {
-        public List<Burger> Burgers { get; set; } = new();
-        public List<Menu> Menus { get; set; } = new();
-        public string? Filter { get; set; }
-    }
-}
