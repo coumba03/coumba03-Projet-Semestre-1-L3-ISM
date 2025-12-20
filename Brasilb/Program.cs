f09420ee13ea444cc9f30fc5557426e6affae48d
coumba03 <mame-coumba.sall@ism.edu.sn>
2025-12-20 20:44:39 +0000

    Ajouter Program.cs

---
+++ Brasilb/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Brasilb.Data.BrasilbDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.LogoutPath = "/Account/Logout";
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+    options.IdleTimeout = TimeSpan.FromHours(4);
+});
+
+builder.Services.AddScoped<Brasilb.Services.IAuthService, Brasilb.Services.Impl.AuthService>();
+builder.Services.AddScoped<Brasilb.Services.ICatalogService, Brasilb.Services.Impl.CatalogService>();
+builder.Services.AddScoped<Brasilb.Services.IOrderService, Brasilb.Services.Impl.OrderService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Catalog}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
